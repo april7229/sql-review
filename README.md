4921f6093dc67b130777e56aa9bfaa98b8954533
Edward Manda <lilkups@yahoo.com>
2018-09-06 22:22:26 -0500

    some slack queries

--- README.md
+++ README.md
@@ -1,25 +1,26 @@
 # sql review - writing the database for slack
   1. Write CREATE TABLE statements for tables 
-  * [ ] organization, channel, user, and message.
-  * [ ] users should have these columns, id, name, username, created, updated 
-  * [ ] organization should have these columns id, name, created
-  * [ ] channel should have id, name, organization_id,  created, updated 
-  * [ ] messages should have these columns, id, content, user_id, channel_id
-  * [ ] Add foreign keys needed to some of the tables.
-  * [ ] Join tables - user_channel
+  * [x] organization, channel, user, and message.
+  * [x] users should have these columns, id, name, username, created, updated 
+  * [x] organization should have these columns id, name, created
+  * [x] channel should have id, name, organization_id,  created, updated 
+  * [x] messages should have these columns, id, content, user_id, channel_id, created, updated
+  * [x] Add foreign keys needed to some of the tables.
+  * [x] Join tables - user_channel
     
   2. Write INSERT queries to add information to the databases.
-  * [ ] 15 users, some of the users are Ironman, Spiderman, Thor, Superman....
-  * [ ] 3 organizations, Lambda School, Shield, Avengers, 
-  * [ ] 4 channels, #general, #random, #CS10, #Labs7
+  * [x] 13 users, some of the users are Ironman, Spiderman, Thor, Superman, Viper, Storm....
+  * [x] 3 organizations, Lambda School, Shield, Avengers, Hydra, X-men
+  * [x] 4 channels, #general, #random, #CS10, #Labs7,
+  * [x] 13 messages
    
   3. Make some SELECT queries to 
-  * [ ] List all organization names.
-  * [ ] List all channel names.
-  * [ ] List all channels in a specific organization by organization name.
-  * [ ] List all messages in a specific channel by channel name #general in order of post_time, descending.
-  * [ ] List all channels to which user Superman belongs.
-  * [ ] List all users that belong to channel #avengers.
-  * [ ] List all messages in all channels by user Ironman.
-  * [ ] List all messages in #avengers by user Thor.
-  * [ ] List the count of messages across all channels per user.
+  * [x] List all organization names.
+  * [x] List all channel names.
+  * [x] List all channels in a specific organization by organization name.
+  * [x] List all messages in a specific channel by channel name #general in order of post_time, descending.
+  * [x] List all channels to which user Ironman belongs.
+  * [x] List all users that belong to channel #general.
+  * [x] List all messages in all channels by user Ironman.
+  * [x] List all messages in #general by user Ironman.
+  * [x] List the count of messages across all channels per user.
---
+++ query.sql
@@ -0,0 +1,258 @@
+
+-- Organization table --
+CREATE TABLE organization (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name VARCHAR
+(64) NOT NULL UNIQUE,
+  created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- user --
+CREATE TABLE user (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name VARCHAR
+(64) NOT NULL UNIQUE,
+  username VARCHAR
+(32) NOT NULL UNIQUE,
+  created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- channel --
+CREATE TABLE channel (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name VARCHAR
+(64) NOT NULL UNIQUE,
+  organization_id INTEGER REFERENCES organization
+(id),
+  created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- message --
+CREATE TABLE message (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  content TEXT,
+  user_id INTEGER REFERENCES user
+(id),
+  channel_id INTEGER REFERENCES channel
+(id),
+  created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- user_channel --
+CREATE TABLE user_channel
+(
+  user_id INTEGER REFERENCES user(id),
+  channel_id INTEGER REFERENCES channel(id)
+);
+
+-- start putting in the data into the database --
+-- users --
+INSERT INTO user
+  (name, username)
+VALUES("Brandon", "hopperb");
+INSERT INTO user
+  (name, username)
+VALUES("Bunthoeun", "b");
+INSERT INTO user
+  (name, username)
+VALUES("Iqra", "ij");
+INSERT INTO user
+  (name, username)
+VALUES("Amy", "crazycatlady");
+INSERT INTO user
+  (name, username)
+VALUES("April", "agm");
+INSERT INTO user
+  (name, username)
+VALUES("Derrick", "dotsing");
+INSERT INTO user
+  (name, username)
+VALUES("Dante", "dantecuales");
+INSERT INTO user
+  (name, username)
+VALUES("Ironman", "tony");
+INSERT INTO user
+  (name, username)
+VALUES("Spiderman", "parker");
+INSERT INTO user
+  (name, username)
+VALUES("Thor", "god");
+INSERT INTO user
+  (name, username)
+VALUES("Viper", "misshydra");
+INSERT INTO user
+  (name, username)
+VALUES("Storm", "water");
+
+-- organizations --
+INSERT INTO organization
+  (name)
+VALUES("Lambda School");
+INSERT INTO organization
+  (name)
+VALUES("Avengers");
+INSERT INTO organization
+  (name)
+VALUES("X-Men");
+INSERT INTO organization
+  (name)
+VALUES("Hydra");
+INSERT INTO organization
+  (name)
+VALUES("Shield");
+
+-- channels --
+INSERT INTO channel
+  (name, organization_id)
+VALUES("#general", 1);
+INSERT INTO channel
+  (name, organization_id)
+VALUES("#random", 4);
+INSERT INTO channel
+  (name, organization_id)
+VALUES("#cs10", 2);
+INSERT INTO channel
+  (name, organization_id)
+VALUES("#labs", 3);
+INSERT INTO channel
+  (name, organization_id)
+VALUES("#after-hours", 5);
+
+-- messages --
+INSERT INTO message
+  (content, user_id, channel_id)
+VALUES("I'M THE KING OF THE WORLD", 1, 5);
+INSERT INTO message
+  (content, user_id, channel_id)
+VALUES("OH, you did an actual query.  That’s awesome lol", 3, 4);
+INSERT INTO message
+  (content, user_id, channel_id)
+VALUES("Cats are cool.", 2, 3);
+INSERT INTO message
+  (content, user_id, channel_id)
+VALUES("So many repos.", 6, 5);
+INSERT INTO message
+  (content, user_id, channel_id)
+VALUES("Don't worry guys.", 5, 2);
+INSERT INTO message
+  (content, user_id, channel_id)
+VALUES("Why do I type so slow?", 7, 4);
+INSERT INTO message
+  (content, user_id, channel_id)
+VALUES("Two hours of sleep is rough.", 4, 1);
+INSERT INTO message
+  (content, user_id, channel_id)
+VALUES("What's the repo for today?", 2, 5);
+INSERT INTO message
+  (content, user_id, channel_id)
+VALUES("Can you make a PR request please", 8, 3);
+INSERT INTO message
+  (content, user_id, channel_id)
+VALUES("Do you think I will graduate?", 9, 4);
+INSERT INTO message
+  (content, user_id, channel_id)
+VALUES("Are we doing #after-hours?", 10, 2);
+INSERT INTO message
+  (content, user_id, channel_id)
+VALUES("Don’t forget to put your name on your pull request.", 11, 3);
+INSERT INTO message
+  (content, user_id, channel_id)
+VALUES("Please take a moment to reflect upon your day and fill out your Daily Standup report: https:\/\/airtable.com\/shr8ZYuNjevMLRsxI", 1, 2);
+
+-- channels --
+INSERT INTO user_channel
+  (user_id, channel_id)
+VALUES(9, 2);
+INSERT INTO user_channel
+  (user_id, channel_id)
+VALUES(4, 1);
+
+-- select organization names only --
+SELECT name AS COOL
+from organization;
+
+--  List all channels in a specific organization by organization name.--
+SELECT channel.name AS "Channel Name"
+FROM channel, organization
+WHERE channel.organization_id = organization.id AND
+  organization.name = "Lambda School";
+
+-- refactored from above --
+-- when two tables have the same column names, 
+-- we have to specify the table we are querying from ---
+SELECT channel.name
+FROM channel, organization
+WHERE organization_id = organization.id
+  AND organization.name = "Lambda School";
+
+-- List all messages from specific--
+SELECT content
+FROM message aAS
+"GENERAL MESSAGES" WHERE channel_id IS
+(SELECT id
+FROM channel
+WHERE name IS '#general')
+ORDER BY created DESC;
+
+-- List all channels ironman belongs to --
+
+SELECT c.name AS "SUPERMANS CHANNEL", c.id AS "CHANNEL ID"
+FROM channel AS c INNER JOIN user_channel AS uc
+  ON c.id
+IS uc.channel_id INNER JOIN user AS u ON u.id IS uc.user_id 
+WHERE u.name IS "Ironman";
+
+-- List all channels ironman belongs to  refactored --
+SELECT channel.name as "Ironman Channels"
+FROM channel INNER JOIN user_channel
+  ON channel.id
+IS user_channel.channel_id INNER JOIN user ON user.id IS user_channel.user_id
+AND user.name = "Ironman";
+
+-- List all users that belong to channel avengers --
+SELECT user.name
+FROM user
+  INNER JOIN user_channel
+  ON user.id
+IS user_channel.user_id 
+INNER JOIN channel 
+ON user_channel.channel_id IS channel.id 
+WHERE channel.name IS "#general";
+
+-- List all messages by ironman from all channels--
+SELECT content
+FROM message
+  INNER JOIN user
+  ON message.user_id
+IS user.id
+WHERE user.name IS "Ironman";
+
+
+-- List all messages in #general by ironman --
+SELECT content
+FROM message
+WHERE channel_id
+IS
+(
+  SELECT id
+FROM channel
+WHERE name is "#general"
+)
+AND user_id IS
+(
+  SELECT id
+FROM user
+WHERE name is "Iqra"
+);
+
+SELECT COUNT(user.id) AS "MESSAGE COUNT", user.name AS "Name"
+FROM message
+  INNER JOIN user
+  ON message.user_id
+IS user.id 
+INNER JOIN channel 
+WHERE message.channel_id IS channel.id 
+GROUP BY user.id;
